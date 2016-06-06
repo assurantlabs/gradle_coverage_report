7fbe2fd5eaae70df30b5c20d8fc2afe2eebf57b4
Jeremie Long <jeremie.long@mobiledefense.com>
2016-06-06 17:29:01 -0400

    Ignore failing tests for coverage reports and print file location

--- src/main/groovy/UnitTestCoverageReport.groovy
+++ src/main/groovy/UnitTestCoverageReport.groovy
@@ -6,6 +6,7 @@ import com.android.build.gradle.api.ApplicationVariant
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.VerificationTask
 import org.gradle.testing.jacoco.tasks.JacocoReport
 
 class UnitTestCoverageReport implements Plugin<Project> {
@@ -27,7 +28,7 @@ class UnitTestCoverageReport implements Plugin<Project> {
             AppExtension android = project.android
 
             android.applicationVariants.all { ApplicationVariant variant ->
-                project.task("create${variant.name.capitalize()}UnitTestCoverageReport", type: JacocoReport, dependsOn: "test${variant.name.capitalize()}UnitTest") {
+                project.task("create${variant.name.capitalize()}UnitTestCoverageReport", type: JacocoReport, dependsOn: ["clean", "test${variant.name.capitalize()}UnitTest"]) {
 
                     def jacocoExcludes = [
                             'android/**',
@@ -55,6 +56,32 @@ class UnitTestCoverageReport implements Plugin<Project> {
                     }
 
                     ext.reportDir = "${project.buildDir}/reports/testsCoverage/${variant.name}/html"
+                }
+            }
+        }
+
+        project.gradle.taskGraph.whenReady {
+            if (project.gradle.taskGraph.allTasks.find { it.name =~ /^create.*CoverageReport$/ } != null) {
+                project.gradle.taskGraph.allTasks.each { task ->
+                    if (task instanceof VerificationTask) {
+                        ((VerificationTask)task).ignoreFailures = true
+                        System.out.println("NOTE: Ignoring test failures for ${task.name} while creating code coverage report")
+                    }
+                }
+
+                // Since the code coverage task silently writes to the build outputs without
+                // mentioning where it wrote, or really that it even wrote anything at all
+                // do our fellow developers a favor and let them know where to find the reports.
+                project.gradle.taskGraph.allTasks.each { task ->
+                    def isCoverageReport = task.name =~ /^create.*CoverageReport$/
+                    if (isCoverageReport) {
+                        task << {
+                            println "##################################################"
+                            println '# The code coverage report can be found at:'
+                            println "# file://${task.reportDir}/index.html"
+                            println "##################################################"
+                        }
+                    }
                 }
             }
         }
